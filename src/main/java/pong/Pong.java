4edb0195d085f923ea3fca7b93882e6d9380d9f5
TomasHoffer2002 <tomihoffer2002@gmail.com>
2025-05-14 17:42:54 -0300

    clase Pong

---
+++ src/main/java/pong/Pong.java
@@ -0,0 +1,29 @@
+package pong;
+
+import com.entropyinteractive.JGame;
+
+import java.awt.*;
+
+public class Pong extends JGame {
+
+    public Pong(String title, int width, int height) {
+        super(title, width, height);
+    }
+
+    public void gameStartup(){
+
+    }
+
+    public void gameUpdate(double var1){
+
+    }
+
+    public void gameDraw(Graphics2D var1){
+
+    }
+
+    public void gameShutdown(){
+
+    }
+
+}
