9415590f31da63abd9cdf5705314d67d7feccfbb
AbrilI2003 <abril.idoeta@gmail.com>
2025-05-14 20:06:51 -0300

    Intento marcador N°1

--- src/main/java/pong/Arco.java
+++ src/main/java/pong/Arco.java
@@ -1,21 +1,29 @@
 package pong;
+
 import clasesCompartidas.ObjetoGrafico;
 
 class Arco extends ObjetoGrafico {
     private int ancho;
     private int alto;
+    private Marcador MarcadorAsociado; 
 
-    public  Arco(int ancho,int alto,double posicionX,double posicionY){
+    public  Arco(int ancho,int alto,double posicionX,double posicionY, Marcador MarcadorAsociado){
         this.ancho=ancho;
         this.alto=alto;
         this.posicionX=posicionX;
         this.posicionY=posicionY;
+        this.MarcadorAsociado=MarcadorAsociado;
     }
     public boolean detectaGol(Pelota pelota) {
-        return pelota.getX() + pelota.getAncho() > this.posicionX &&
-                pelota.getX() < this.posicionY + ancho &&
-                pelota.getY() + pelota.getAlto() > this.posicionY &&
-                pelota.getY() < this.posicionY + alto;
+        boolean gol= false;
+        if(pelota.getX() + pelota.getAncho() > this.posicionX &&
+            pelota.getX() < this.posicionY + ancho &&
+            pelota.getY() + pelota.getAlto() > this.posicionY &&
+            pelota.getY() < this.posicionY + alto){
+                gol= true;
+                MarcadorAsociado.incrementarPuntaje();
+            }
+        return gol;
     }
     @Override
     public void update(double delta){}
--- src/main/java/pong/Marcador.java
+++ src/main/java/pong/Marcador.java
@@ -1,4 +1,23 @@
 package pong;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
 class Marcador {
+    private int puntaje;
+    public void Marcador(){
+        puntaje = 0;
+    }
+
+    public void incrementarPuntaje() {
+        puntaje++;
+    }
+
+    public void dibujar(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 30));
+        g.drawString(String.valueOf(puntosJugadorIzquierda), 100, 50);
+        g.drawString(String.valueOf(puntosJugadorDerecha), 500, 50);
+    }
 }
