9bdf2c959665c2ced96641398ef1374932fadcda
AbrilI2003 <abril.idoeta@gmail.com>
2025-06-03 19:13:02 -0300

    Sprites nuevos de Paracaidista y Bloqueador

--- src/main/java/lemmings/Bloqueador.java
+++ src/main/java/lemmings/Bloqueador.java
@@ -1,10 +1,11 @@
 package lemmings;
 
 
-import javax.imageio.ImageIO;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Bloqueador extends Bichito {
     private BufferedImage[] frames;
@@ -22,17 +23,42 @@ public class Bloqueador extends Bichito {
     }
 
     private void cargarFrames(BufferedImage spriteSheet) {
-        int ancho = 16;
-        int alto = 12; // altura estimada del Lemming bloqueador
-        int cantidadFrames = 16;
-        int escala = 2;
-        int fila = 12; // fila 13 en base 0
+        //Matriz de coordenadas para los sprites
+        int[][] coordenadas = {
+            {20, 148, 10, 10}, 
+            {36, 148, 10, 10}, 
+            {52, 148, 10, 10}, 
+            {68, 148, 10, 10}, 
+            {84, 148, 10, 10}, 
+            {100, 148, 10, 10}, 
+            {116, 148, 10, 10}, 
+            {132, 148, 10, 10}, 
+            {148, 148, 10, 10}, 
+            {164, 148, 10, 10}, 
+            {180, 148, 10, 10}, 
+            {196, 148, 10, 10}, 
+            {212, 148, 10, 10}, 
+            {228, 148, 10, 10}, 
+            {244, 148, 10, 10}, 
+            {260, 148, 10, 10}, 
+        };
+
+        int escala=2;
+        int cantidadFrames= coordenadas.length;
 
         frames = new BufferedImage[cantidadFrames];
 
         for (int i = 0; i < cantidadFrames; i++) {
-            BufferedImage frame = spriteSheet.getSubimage((i + 1) * ancho, fila * alto + 4, ancho, alto);
-            frames[i] = escalarImagen(frame, ancho * escala, alto * escala);
+            int x = coordenadas[i][0];
+            int y = coordenadas[i][1];
+            int ancho = coordenadas[i][2];
+            int alto = coordenadas[i][3];
+
+            frames[i] = escalarImagen(
+                    spriteSheet.getSubimage(x, y, ancho, alto),
+                    ancho * escala,
+                    alto * escala
+            );
         }
     }
 
